0f41e87f5a1f11b20986efc4741e984ae80b8adf
Salvatore D'Agostino <toto19@gmail.com>
2016-06-03 20:13:54 -0700

    fix import and path to test audio file

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"github.com/iToto/audicut/soxCLI"
+	"github.com/iToto/goSoxCLI/soxCLI"
 	"log"
 )
 
 func main() {
-	path := "/Users/iToto/Dev/golang/src/github.com/iToto/audicut/audio/test.wav"
+	path := "audio/test.wav"
 	file, err := soxCLI.GetTrackInfo(path)
 
 	if err != nil {
